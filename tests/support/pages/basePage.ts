ab02373afd702cb2264298b5863f5a51af189f3c
Aleksey Lavrukhin <alexudracul@gmail.com>
2025-04-22 16:27:35 +0300

    move navigationSection to basePage

--- tests/support/pages/basePage.ts
+++ tests/support/pages/basePage.ts
@@ -1,6 +1,24 @@
 import { Locator, Page } from '@playwright/test';
-import { NavigationSection } from '../sections/navigationSection';
 import { chatSection } from '../sections/chatSection';
+
+export class NavigationSection {
+  readonly projects: Locator;
+  readonly tasks: Locator;
+  readonly activity: Locator;
+  readonly contacts: Locator;
+  readonly help: Locator;
+  readonly myAccount: Locator;
+
+  constructor(private page: Page) {
+    this.page = page;
+    this.projects = page.getByRole('link', { name: 'Projects' });
+    this.tasks = page.getByRole('link', { name: 'My Tasks' });
+    this.activity = page.getByRole('link', { name: 'Activity' });
+    this.contacts = page.getByRole('link', { name: 'Contacts' });
+    this.help = page.getByRole('link', { name: 'Help' });
+    this.myAccount = page.getByRole('link', { name: 'My Account' });
+  };
+};
 
 export class BasePage {
   readonly page: Page;
